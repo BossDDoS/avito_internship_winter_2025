c2b67add849369b518f045d209451e71b0d557e8
Maxim L <maksim.ldokov.97@mail.ru>
2025-02-13 22:41:14 +0400

    Add endpoint

--- src/modules/posts/containers/PostListContainer.tsx
+++ src/modules/posts/containers/PostListContainer.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useGetPostsQuery } from '../models/api';
 
 export function PostListContainer() {
+  const { data } = useGetPostsQuery();
+
+  console.log(data);
+
   return <div>PostListContainer</div>;
 }
--- src/modules/posts/models/api.ts
+++ src/modules/posts/models/api.ts
@@ -1,5 +1,13 @@
 import { baseApi } from 'store/baseApi';
 
 export const postsApi = baseApi.injectEndpoints({
-  endpoints: () => ({}),
+  endpoints: (build) => ({
+    getPosts: build.query<object, void>({
+      query: () => 'list',
+    }),
+  }),
 });
+
+// eslint-disable-next-line
+// @ts-ignore
+export const { useGetPostsQuery } = postsApi;
